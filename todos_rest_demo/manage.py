b7b3ae936ddf14a4d47c965c3308fa8bd0d78f20
Doncho Minkov <donchominkov@gmail.com>
2023-07-26 21:57:51 +0300

    Added demo for the React/DRF app

--- todos_rest_demo/manage.py
+++ todos_rest_demo/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auth_demos.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todos_rest_demo.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,6 +16,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
 
 if __name__ == '__main__':
     main()
